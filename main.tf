1f083e7bec7140b795f018580b4ef6f158a842e3
Kostas Sarikioses <k.sarikioses@gmail.com>
2023-09-25 22:27:12 +0000

    #5 Added random provider, S3 bucket and randomised part of its name

--- main.tf
+++ main.tf
@@ -1,1 +1,33 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.16.2"
+    }
+  }
+}
 
+provider "aws" {
+}
+provider "random" {
+  # Configuration options
+}
+
+resource "random_string" "bucket_random_string" {
+  lower = true
+  upper = false
+  special  = false
+  length   = 6
+}
+
+resource "aws_s3_bucket" "static_website_bucket" {
+  bucket = format("%s%s","terraform-beginner-bootcamp-kostassar-",random_string.bucket_random_string.result)
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.static_website_bucket.id
+}
