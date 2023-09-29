3c607dc9debfe08c9667088efd1302434cfab50f
Kostas Sarikioses <k.sarikioses@gmail.com>
2023-09-29 22:28:23 +0000

    #23 Removed random provider, added Terraform import documetantion.

--- main.tf
+++ main.tf
@@ -1,12 +1,5 @@
-resource "random_string" "bucket_random_string" {
-  lower = true
-  upper = false
-  special  = false
-  length   = 16
-}
-
 resource "aws_s3_bucket" "static_website_bucket" {
-  bucket = format("%s%s","terraform-beginner-bootcamp-",random_string.bucket_random_string.result)
+  bucket = var.website_bucket_name
   tags = {
     UserUuid = var.user_uuid
   }
