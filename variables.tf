8ee528af04abe36d6bbfe20f52f6437285b04719
Kostas Sarikioses <k.sarikioses@gmail.com>
2023-09-29 22:30:51 +0000

    Fixed variable referencing

--- variables.tf
+++ variables.tf
@@ -13,8 +13,8 @@ variable "website_bucket_name" {
 
   validation {
     condition = (
-      length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63 &&
-      can(regex("^[a-z0-9][a-z0-9-.]*[a-z0-9]$", var.bucket_name))
+      length(var.website_bucket_name) >= 3 && length(var.website_bucket_name) <= 63 &&
+      can(regex("^[a-z0-9][a-z0-9-.]*[a-z0-9]$", var.website_bucket_name))
     )
     error_message = "The bucket name must be between 3 and 63 characters, start and end with a lowercase letter or number, and can contain only lowercase letters, numbers, hyphens, and dots."
   }
