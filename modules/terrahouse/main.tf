e68c89c7b82a005c3769e03e6c4687369c1afd9a
Kostas Sarikioses <k.sarikioses@gmail.com>
2023-10-01 16:02:57 +0000

    #27 Added static web hosting to S3

--- modules/terrahouse/main.tf
+++ modules/terrahouse/main.tf
@@ -4,3 +4,31 @@ resource "aws_s3_bucket" "static_website_bucket" {
     UserUuid = var.user_uuid
   }
 }
+
+resource "aws_s3_bucket_website_configuration" "static_website_configuration" {
+  bucket = aws_s3_bucket.static_website_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_object" "index_html" {
+  bucket = aws_s3_bucket.static_website_bucket.bucket
+  key    = "index.html"
+  source = var.index_path
+
+  etag = filemd5(var.index_path)
+}
+
+resource "aws_s3_object" "error_html" {
+  bucket = aws_s3_bucket.static_website_bucket.bucket
+  key    = "error.html"
+  source = var.error_path
+
+  etag = filemd5(var.error_path)
+}
